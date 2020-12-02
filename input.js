82e5ec3e1c6efff96473a86b719a8ef93d0d9ac2
Kelechi Obikaonu <kelechipauline@gmail.com>
2020-12-02 00:32:51 +0000

    modified the play and input files

--- input.js
+++ input.js
@@ -1,15 +1,37 @@
-const setupInput = function() {
-  const stdin =  process.stdin;
+const { connect } = require("./client");
+
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn
+  const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  
-  const handlerUserInput = stdin.on('data', () => {
-    if (key === '/u0003') {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
       process.exit();
+
     }
+    handlerUserInput(key)
   })
+
   return stdin;
 }
 
-module.exports = setupInput;
+const handlerUserInput = function (key) {
+  if (key === 'w') {
+    connection.write('Move: up')
+  }
+  if (key === 'a') {
+    connection.write('Move: left')
+  }
+  if (key === 's') {
+    connection.write('Move: down')
+  }
+  if (key === 'd') {
+    connection.write('Move: right')
+  }
+}
+
+module.exports = { setupInput };
